b3f58d6128436011a36e0c4890a8a893fb95f39f
DevAbidHasan <abidhasanplabon80@gmail.com>
2025-09-04 16:03:24 +0600

    project set up done: tailwind, daisyui, font

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className='font-black text-center my-5 poppins text-2xl'>Plant Pal : Plant Tracking Website</h2>
+      <h2 className='text-green-500 inter text-2xl'>Plant Pal : Plant Tracking Website</h2>
+      
+    </>
+  )
+}
+
+export default App
